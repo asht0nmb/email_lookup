c0b22b337f94fd9f155eb8c02e3c293882a8e7e9
Ashton Meyer-Bibbins <ashtonmb@uw.edu>
2025-05-16 19:26:47 -0700

    Update config.template.py

--- config.template.py
+++ config.template.py
@@ -1,17 +1,17 @@
 
-# 1) URLs
+# 1) File paths
+# Path to your input CSV (with a “Name” column)
+NAMES_FILE        = 'path/to/your/input.csv' 
+# Path to where you want the output written
+OUTPUT_FILE       = 'path/to/your/output.csv'
+
+# 2) URLs
 LOGIN_URL          = 'https://my.univ.edu/login'
 SEARCH_URL         = 'https://my.univ.edu/directory'
 
-# 2) CSS selectors
+# 3) CSS selectors
 SIGNIN_BUTTON_SEL       = '#sign-in'
 SEARCH_INPUT_SEL        = '#query'
 STUDENTS_ONLY_LABEL_SEL = '.population-filter input[value="students"]'
 RESULT_ROW_SEL          = 'table.results tbody tr'
 EMAIL_CELL_SEL          = 'td.email-column'
-
-# 3) File paths
-# Path to your input CSV (with a “Name” column)
-NAMES_FILE        = 'path/to/your/input.csv' 
-# Path to where you want the output written
-OUTPUT_FILE       = 'path/to/your/output.csv'
